a4c4cda44268ff3b461662a100fd745cd900ce5c
BrayanC17 <bcastrovarela@ucompensar.edu.co>
2026-03-13 01:19:33 -0500

    creacion entidad usuario

---
+++ EsportsArena.Entities/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EsportsArena.Entities.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        
+        public virtual ICollection<UsuarioRol> UsuarioRoles { get; set; }
+    }
+}
