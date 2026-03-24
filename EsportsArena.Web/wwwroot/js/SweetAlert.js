d8129cfc9a068e49b50ad308db02d91dd271ba6c
Dev Samuel <junielessamuel@gmail.com>
2026-03-23 20:02:52 -0500

    Cambios Front-End y agregado Alertas

---
+++ EsportsArena.Web/wwwroot/js/SweetAlert.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const loginError = document.getElementById("loginErrorData")?.value;
+
+    if (loginError) {
+        Swal.fire({
+            icon: 'error',
+            title: '¡ACCESO DENEGADO!',
+            text: loginError,
+            background: '#1a1a1a',
+            color: '#fff',
+            confirmButtonColor: '#ffc107',
+            confirmButtonText: 'REINTENTAR',
+            customClass: {
+                popup: 'border-warning shadow-lg'
+            }
+        });
+    }
+});
