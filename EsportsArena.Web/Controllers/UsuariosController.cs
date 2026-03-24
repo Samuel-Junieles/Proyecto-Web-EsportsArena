05d8f7fe2fe4a26b3fc5daea89ceb1c25ea5c97e
Dev Samuel <junielessamuel@gmail.com>
2026-03-23 22:47:25 -0500

    Remake Usuario y Agregado DAO Funcional

--- EsportsArena.Web/Controllers/UsuariosController.cs
+++ EsportsArena.Web/Controllers/UsuariosController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using EsportsArena.Logic.Services; 
 using Microsoft.AspNetCore.Http;
+using EsportsArena.Entities.Models;
 
 namespace EsportsArena.Web.Controllers
 {
@@ -24,6 +25,45 @@ namespace EsportsArena.Web.Controllers
             // Obtenemos los datos de MySQL
             var listaUsuarios = _usuarioService.ObtenerTodosLosUsuarios();
             return View(listaUsuarios);
+        }
+
+        [HttpPost]
+        public IActionResult Actualizar([FromBody] Usuario datosNuevos)
+        {
+            // 1. Buscamos el usuario real que está en XAMPP actualmente
+            var usuarioDB = _usuarioService.ObtenerPorId(datosNuevos.Id);
+
+            if (usuarioDB == null)
+                return Json(new { success = false, message = "Usuario no encontrado" });
+
+            // 2. MAPEAMOS SOLO LOS CAMPOS PERMITIDOS
+            // Esto protege el PasswordHash y el RolId porque nunca los tocamos
+            usuarioDB.Username = datosNuevos.Username;
+            usuarioDB.Email = datosNuevos.Email;
+
+            // 3. Guardamos el objeto que ya tiene su Password y Rol intactos
+            var exito = _usuarioService.Actualizar(usuarioDB);
+
+            if (exito)
+                return Json(new { success = true, message = "Perfil actualizado correctamente" });
+
+            return Json(new { success = false, message = "Error al persistir cambios" });
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(int id)
+        {
+            // Llamamos al método real del Service que creamos antes
+            bool exito = _usuarioService.Eliminar(id);
+
+            if (exito)
+            {
+                return Json(new { success = true, message = "Usuario eliminado de la base de datos." });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Error: No se pudo eliminar el registro en XAMPP." });
+            }
         }
     }
 }
